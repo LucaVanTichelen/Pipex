21d217f1e7c64cf2c8d610dd5f14f7e74189b174
Luca Van tichelen <luca_van_tic@hotmail.com>
2022-02-12 08:58:41 +0100

    Child functions done

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: lvan-tic <lvan-tic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/06 15:08:28 by lvan-tic          #+#    #+#             */
-/*   Updated: 2022/02/07 16:09:37 by lvan-tic         ###   ########.fr       */
+/*   Updated: 2022/02/11 11:29:22 by lvan-tic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,52 @@ char	**parse_paths(char **envp)
 	return (paths);
 }
 
+void	child1(int *fd, char **argv, char **envp, char **paths)
+{
+	int		i;
+	int		infile;
+	char	**args;
+	char 	*cmd;
+
+	i = 0;
+	infile = open(argv[1], O_RDONLY);
+	args = ft_split(argv[2], ' ');
+	close(fd[0]);
+	dup2(infile, 0);
+	dup2(fd[1], 1);
+	close(infile);
+	close(fd[1]);
+	while (paths[i])
+	{
+		cmd = ft_strjoin(paths[i], argv[2]);
+		execve(cmd, args, envp);
+		i++;
+	}
+}
+
+void	child2(int *fd, char **argv, char **envp, char **paths)
+{
+	int		i;
+	int		outfile;
+	char	**args;
+	char	*cmd;
+
+	i = 0;
+	outfile = open(argv[4], O_WRONLY, O_CREAT);
+	args = ft_split(argv[3], ' ');
+	close(fd[1]);
+	dup2(fd[0], 0);
+	dup2(outfile, 1);
+	close(fd[0]);
+	close(outfile);
+	while (execve(paths[i], args, envp))
+	{
+		cmd = ft_strjoin(paths[i], argv[3]);
+		execve(cmd, args, envp);
+		i++;
+	}
+}
+
 int	main(int argc, char **argv, char **envp)
 {
 	int		fd[2];
@@ -39,11 +85,28 @@ int	main(int argc, char **argv, char **envp)
 	int		pid2;
 	char	**paths;
 
+	if (argc != 5)
+		return (0);
 	paths = NULL;
 	paths = parse_paths(envp);
 	pipe(fd);
 	pid1 = fork();
-	if (pid1 > 0)
+	if (pid1 == 0)
+	{
+		child1(fd, argv, envp, paths);
+	}
+	else
+	{
+		waitpid(pid1, NULL, 0);
 		pid2 = fork();
+		if (pid2 == 0)
+			child2(fd, argv, envp, paths);
+		else
+		{
+			close(fd[0]);
+			close(fd[1]);
+			waitpid(pid2, NULL, 0);
+		}
+	}
 	return (0);
 }
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: lvan-tic <lvan-tic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 15:27:39 by lvan-tic          #+#    #+#             */
-/*   Updated: 2022/02/07 15:27:40 by lvan-tic         ###   ########.fr       */
+/*   Updated: 2022/02/11 11:19:15 by lvan-tic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 # include <string.h>
 # include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
 
 char	**ft_malloc(char **str, int *tab, int x);
 char	**ft_fill_split(char const *s, char c, char **str, int *tab);
@@ -31,5 +33,9 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 
 char	*ft_fill(char *str, char const *s, unsigned int start, size_t len);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+char	**parse_paths(char **envp);
+void	child1(int *fd, char **argv, char **envp, char **paths);
+void	child2(int *fd, char **argv, char **envp, char **paths);
 
 #endif
